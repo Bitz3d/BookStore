3b50dfc4b5318fa9b3f720e90813acbdb0a06ecd
RafaU <raf.cieslinski@gmail.com>
2018-06-08 17:05:29 +0200

    repository

--- src/main/java/pl/rafalab/repository/Bookrepository.java
+++ src/main/java/pl/rafalab/repository/Bookrepository.java
@@ -3,15 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pl.rafalab.model;
+package pl.rafalab.repository;
 
 /**
  *
  * @author Rafal
  */
-public enum Language {
-
-ENGLISH, FRENCH, POLISH, SPANISH, ITALIAN, GERMAN, RUSSIAN
-
+public class Bookrepository {
     
 }
